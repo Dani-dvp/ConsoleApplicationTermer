2ad4ad44cdb4022f4a65b198640f9c5b268dd9ed
Dani-dvp <kevin1995dani@gmail.com>
2020-11-13 15:54:35 +0100

    Third Commit

--- ConsoleApplicationTermer/Program.cs
+++ ConsoleApplicationTermer/Program.cs
@@ -15,9 +15,9 @@ namespace ConsoleApplicationTermer
             {
                 termer.HandleInput();
                 Console.ReadLine(); // La till den så att man kan läsa resultatet innan den frågar om man vill avsluta.
-                Console.WriteLine("Vill du fortsätta?");
-                Console.WriteLine("1. Ja.");
-                Console.WriteLine("2. Nej jag vill avsluta programmet.");
+                Console.WriteLine("> Vill du fortsätta?");
+                Console.WriteLine("> 1. Ja.");
+                Console.WriteLine("> 2. Nej jag vill avsluta programmet.");
                 string avsluta = Console.ReadLine();
                 if (avsluta == "2")
                 {
